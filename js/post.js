12d6db192970c7cd7dace569c00afca49bf8755e
Corbin Muraro <corbinmuraro@gmail.com>
2017-02-07 19:11:23 -0800

    Finish cousteau article, small edits, reorganize files

--- js/post.js
+++ js/post.js
@@ -4,23 +4,9 @@ $(function() {
 		$(this).toggleClass('magnified');
 	});
 
-    // $('video').hover(function() {
-    // 	$(this)[0].play();
-    // 	$(this).css('opacity', '1');
-    // }, function() {
-    // 	$(this)[0].pause();
-    // 	$(this)[0].currentTime = 0;
-    // 	$(this).css('opacity', '0.3');
-    // });
-    //  $(window).scroll(function() {
-    //     $('video').each(function(){
-    //         if ($(this).is(":in-viewport")) {
-    //             $(this)[0].play();
-    //         } else {
-    //             $(this)[0].pause();
-    //         }
-    //     });
-    // });
-
-
+    $("video").hover(function() {
+        return $(this).css("opacity", "1"), this.play();
+    }, function() {
+        return $(this).css("opacity", ".3"), this.pause();
+    });
 });
