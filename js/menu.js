9499ab59117b1a5e1fe82c957edbfb0d7aa01fa0
Corbin Muraro <corbinmuraro@gmail.com>
2017-11-24 19:55:27 -0800

    Fix mobile dropdown! also /blog spacing tweaks

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,23 @@ window.addEventListener("load", () => {
 	let navButton = document.getElementById("nav-button");
 	let navContainer = document.getElementById("nav-container");
 	navButton.onclick = function() {
-		console.log('clicked');
-    	if (navContainer.className === "") {
-    		console.log('opening');
-    		navContainer.className = "active";
+    	if (navContainer.classList.contains("active")) {
+    		console.log('closing');
+    		navContainer.classList.remove("active");
     	}
     	else {
-			console.log('closing');
-    		navContainer.className = "";
+			console.log('opening');
+    		navContainer.classList.add("active");
     	}
 	};
 });
+
+window.onclick = function(event) {
+	let navContainer = document.getElementById("nav-container");
+	if (!event.target.matches('#nav-button')) {
+		if (navContainer.classList.contains("active")) {
+        	navContainer.classList.remove("active");
+      	}
+
+	}
+}
